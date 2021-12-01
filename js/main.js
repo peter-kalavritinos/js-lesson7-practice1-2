95eca731b91bcb4aa97f630635e7c2fd95f7b679
Peter Kalavritinos <peterkalav1994@gmail.com>
2021-12-01 12:05:02 -0500

    Transfer from sandbox to github

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var box1 = document.querySelector(".box-1");
+var box2 = document.querySelector(".box-2");
+var box3 = document.querySelector(".box-3");
+var button = document.querySelector("button");
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "1") {
+        box1.innerText = "😸";
+    } else if (e.key === "2") {
+        box2.innerText = "🦨";
+    } else if (e.key === "3") {
+        box3.innerText = "🐡";
+    }
+});
+
+button.addEventListener("click", function() {
+    box1.innerText = "1";
+    box2.innerText = "2";
+    box3.innerText = "3";
+});
